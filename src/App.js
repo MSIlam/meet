b99b46683a0422030347c9a1c2e8209068e1e91f
MSIlam <saiful.zoology@yahoo.com>
2023-12-18 14:22:16 +0100

    little modification

--- src/App.js
+++ src/App.js
@@ -67,7 +67,9 @@ const App = () => {
         setCurrentNOE={setCurrentNOE}
         setErrorAlert={setErrorAlert}
       />
-      <CityEventsChart allLocations={allLocations} events={events} />
+      <div className="charts-container">
+        <CityEventsChart allLocations={allLocations} events={events} />
+      </div>
       <EventList events={events} />
       <ToastContainer />
     </div>
