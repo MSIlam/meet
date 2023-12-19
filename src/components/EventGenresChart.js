3eedd6afd251d883f0c371c70b49a3a206ec2ba4
MSIlam <saiful.zoology@yahoo.com>
2023-12-19 01:33:44 +0100

    little modification

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -1,98 +1,3 @@
-// import { useState, useEffect } from "react";
-// import { Fragment } from "react";
-// import {
-//   PieChart,
-//   Pie,
-//   Legend,
-//   Tooltip,
-//   ResponsiveContainer,
-//   Cell,
-// } from "recharts";
-
-// const EventGenresChart = ({ events }) => {
-//   const colors = ["#DD0000", "#00DD00", "#0000DD", "#DDDD00", "#DD00DD"];
-//   const RADIAN = Math.PI / 180;
-//   const renderCustomizedLabel = ({
-//     cx,
-//     cy,
-//     midAngle,
-//     innerRadius,
-//     outerRadius,
-//     percent,
-//     index,
-//   }) => {
-//     const radius = innerRadius + (outerRadius - innerRadius) * 0.7;
-//     const x = cx + radius * Math.cos(-midAngle * RADIAN);
-//     const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-//     return (
-//       <React.Fragment>
-//         <text
-//           x={x}
-//           y={y}
-//           fill="white"
-//           textAnchor={x > cx ? "start" : "end"}
-//           dominantBaseline="central"
-//         >
-//           {`${(percent * 100).toFixed(0)}%`}
-//         </text>
-//         <line
-//           x1={x}
-//           y1={y}
-//           x2={x + (x > cx ? 30 : -30)}
-//           y2={y}
-//           stroke="#666"
-//         />
-//       <React.Fragment/>
-//     );
-//   };
-
-//   const [data, setData] = useState([]);
-//   const genres = ["React", "JavaScript", "Node", "jQuery", "Angular"];
-
-//   useEffect(() => {
-//     setData(getData());
-//   }, [`${events}`]);
-
-//   const getData = () => {
-//     const data = genres.map((genre) => {
-//       const filteredEvents = events.filter((event) =>
-//         event.summary.includes(genre)
-//       );
-//       return {
-//         name: genre,
-//         value: filteredEvents.length,
-//       };
-//     });
-//     return data;
-//   };
-
-//   return (
-//     <ResponsiveContainer width="99%" height={400}>
-//       <PieChart>
-//         <Pie
-//           data={data}
-//           cx="50%"
-//           cy="50%"
-//           labelLine={false}
-//           label={renderCustomizedLabel}
-//           outerRadius={130}
-//           fill="#8884d8"
-//           dataKey="value"
-//         >
-//           {data.map((entry, index) => (
-//             <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
-//           ))}
-//         </Pie>
-//         <Tooltip />
-//         <Legend verticalAlign="bottom" height={36} />
-//       </PieChart>
-//     </ResponsiveContainer>
-//   );
-// };
-
-// export default EventGenresChart;
-
 import { useState, useEffect, Fragment } from "react";
 import { PieChart, Pie, ResponsiveContainer, Cell } from "recharts";
 
@@ -158,14 +63,7 @@ const EventGenresChart = ({ events }) => {
 
   return (
     <ResponsiveContainer width="99%" height={400}>
-      <PieChart
-        margin={{
-          top: 20,
-          right: 20,
-          bottom: 60,
-          left: -20,
-        }}
-      >
+      <PieChart>
         <Pie
           data={data}
           cx="50%"
